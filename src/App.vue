b0de6cc65c9b0580426711879fbd5d85289bead9
Kunniii <nguyenairport56@gmail.com>
2023-02-11 04:35:24 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Dashboard from "./views/dashboard.vue";
+import Login from "./views/login.vue";
+
+const sid = localStorage.getItem("sid");
+</script>
+
+<template>
+  <div class="w-96 mx-auto">
+    <Login />
+  </div>
+</template>
---
+++ src/views/Login.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from "vue";
+
+let sid = ref("");
+const handleLoginClick = () => {
+  console.log(sid.value);
+};
+</script>
+
+<template>
+  <div class="flex flex-col justify-center">
+    <h1>Welcome to FAP Attendance Status</h1>
+
+    <input
+      class="text-center border"
+      type="text"
+      placeholder="Your SessionID..."
+      v-model="sid"
+    />
+    <button @click="handleLoginClick">Continue</button>
+  </div>
+</template>
